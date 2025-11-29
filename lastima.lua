4a97cdf9432adbef49ce8be80dcbf2eda5ec1868
fort <saul827283838@gmail.com>
2025-11-29 11:52:00 -0600

    Update lastima.lua

--- lastima.lua
+++ lastima.lua
@@ -769,7 +769,7 @@ local function createFarmToggle(name, posX, posY, posZ)
                         if not success then
                             warn("Error durante el teletransporte: "..err)
                         end
-                        task.wait(2)
+                        task.wait(00.1)
                     end
                 end
                 spawn(teleportLoop)
